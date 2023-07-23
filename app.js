128fd0172901926f26e86aaf3a12c791ca593863
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-07-23 14:05:24 +0500

    Section4 addded

---
+++ app.js
@@ -0,0 +1,15 @@
+// function showStoreSec() {
+//     var storeSec = document.getElementById("store-sec");
+//     var navBarSec = document.getElementById('navbar-section');
+
+//     navBarSec.style.backgroundColor = "white";
+//     storeSec.style.display = "block";    
+// }
+
+// function hideStoreSec() {
+//     var storeSec = document.getElementById("store-sec");
+//     var navBarSec = document.getElementById('navbar-section');
+
+//     navBarSec.style.backgroundColor = "none";
+//     storeSec.style.display = "none";
+// }
